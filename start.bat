35c02a697cd7cf97d9394c2e6ef6db3ee5a4a6a1
bharatsharma2712 <55744049+bharatsharma2712@users.noreply.github.com>
2025-01-07 09:12:49 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -12,6 +12,7 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
+tasklist | find /i "ngrok.exe" 
 echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
